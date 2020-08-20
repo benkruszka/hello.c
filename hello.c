4c4a7a641cb7ce65b6884a2693dbb58f29cce264
benkruszka <bkruszka2@huskers.unl.edu>
2020-08-20 18:12:09 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Ben Kruszka
+ *         cbourke@cse.unl.edu
+ * Date: 2020/17/08
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Ben Kruszka \n");
+
+  return 0;
+}
